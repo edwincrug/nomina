f15cff81458554d931a307001a8797bfd8e91b5c
GibranQC <gibranqc9131@gmail.com>
2016-12-27 12:29:17 -0600

    Validaciones de carpetas

--- app/website/controllers/filetree.js
+++ app/website/controllers/filetree.js
@@ -9,7 +9,7 @@ var dirTree = require('directory-tree');
 
 
 
-var FileTree = function (conf) {
+var FileTree = function(conf) {
     this.conf = conf || {};
 
     this.view = new FileTreeView();
@@ -17,7 +17,7 @@ var FileTree = function (conf) {
         parameters: this.conf.parameters
     });
 
-    this.response = function () {
+    this.response = function() {
         this[this.conf.funcionalidad](this.conf.req, this.conf.res, this.conf.next);
     };
 };
@@ -27,24 +27,46 @@ var FileTree = function (conf) {
 FileTree.prototype.get_files = function(req, res, next) {
     var self = this;
 
-    var params = [{name: 'idEmpresa',value: req.query.idEmpresa  ,type: self.model.types.STRING},
-                {name: 'idTipo',value: req.query.idTipo ,type: self.model.types.INT}];
+    var params = [{ name: 'idEmpresa', value: req.query.idEmpresa, type: self.model.types.STRING },
+        { name: 'idTipo', value: req.query.idTipo, type: self.model.types.INT }
+    ];
 
-    this.model.query('SEL_RUTAS_ORIGEN_SP', params, function (error, result) {
+    this.model.query('SEL_RUTAS_ORIGEN_SP', params, function(error, result) {
         var trees = dirTree(result[0].RutaDescripcion);
+        var elementos = [];
+        var elementosValidos = [];
+        var elementosFinales = [];
 
+        for (var i = 0; i < trees.children.length; i++) {
+            if (trees.children[i].name.length == 8) {
+                if (trees.children[i].children.length == 0) {
+                    console.log('carpeta vacia')
+                    elementos.push({
+                        fila: i
+                    })
+                } else {
+                    console.log('carpeta con elementos')
+                    elementosValidos.push({
+                        datos: trees.children[i]
+                    })
+                }
+            } else {
+                console.log('tamaño corto')
+            }
+        }
+        for (var h = 0; h < elementosValidos.length; h++) {
+            if ((elementosValidos[h].datos.name.substr(0, 2) <= 31 && elementosValidos[h].datos.name.substr(0, 2) > 0) && (elementosValidos[h].datos.name.substr(2, 2) <= 12 && elementosValidos[h].datos.name.substr(2, 2) > 0) && (elementosValidos[h].datos.name.substr(4, 4) <= 9999 && elementosValidos[h].datos.name.substr(4, 4) > 0)) {
+                elementosFinales.push({ datos: elementosValidos[h].datos })
+            } else {
+                console.log('no')
+            }
+        }
+        console.log(elementosFinales)
         self.view.expositor(res, {
             error: error,
-            result: trees
+            result: elementosFinales
         });
     });
-//     var tree = dirTree('C:/Users/hitma/Documents/Prueba');
-// var trees = dirTree('C:/Users/hitma/Documents/Meta 4/');
-// res.json({
-//             error_code: 0,
-//             err_desc: null,
-//             datos: tree
-//         });
 };
 
 module.exports = FileTree;
