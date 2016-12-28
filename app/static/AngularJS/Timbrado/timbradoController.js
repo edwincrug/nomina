7248888023e2e148127322b7975d25e9251df064
GibranQC <gibranqc9131@gmail.com>
2016-12-27 18:14:31 -0600

    carpetas en arbol

--- app/static/AngularJS/Timbrado/timbradoController.js
+++ app/static/AngularJS/Timbrado/timbradoController.js
@@ -6,46 +6,9 @@ registrationModule.controller('timbradoController', function($scope, $rootScope,
     $scope.init = function() {
         $scope.yo = false;
         openCloseNav()
-        $scope.treeView();
-        $scope.getEmpresa($scope.idUsuario);
+        $scope.getEmpresa(1);
         $scope.getTipoNomina();
-        //$scope.getFileTree();
     }
-
-
-    $scope.treeView = function() {
-        $(document).ready(function() {
-
-            $('#jstree1').jstree({
-                'core': {
-                    'check_callback': true
-                },
-                'plugins': ['types', 'dnd'],
-                'types': {
-                    'default': {
-                        'icon': 'glyphicon glyphicon-folder-open'
-                    },
-                    'html': {
-                        'icon': 'glyphicon glyphicon-file'
-                    },
-                    'svg': {
-                        'icon': 'glyphicon glyphicon-picture'
-                    },
-                    'css': {
-                        'icon': 'glyphicon glyphicon-file'
-                    },
-                    'img': {
-                        'icon': 'glyphicon glyphicon-picture'
-                    },
-                    'js': {
-                        'icon': 'glyphicon glyphicon-file'
-                    }
-
-                }
-            });
-        });
-    }
-
 
     $scope.getEmpresa = function(idUsuario) {
         filtrosRepository.getEmpresa(idUsuario).then(function(result) {
@@ -53,50 +16,31 @@ registrationModule.controller('timbradoController', function($scope, $rootScope,
                 $scope.empresaUsuario = result.data;
             }
         });
-
-        $scope.getTipoNomina = function() {
-                filtrosRepository.getTipoNomina().then(function(result) {
-                    if (result.data.length > 0) {
-                        $scope.tipoNomina = result.data;
-                    }
-                });
-            } //getFileTree
-
-        $scope.getFileTree = function(idEmpresa, idTipo) {
-
-            filetreeRepository.getFileTree(idEmpresa, idTipo).then(function(result) {
-                if (result.data != undefined) {
-                    $scope.filetree = result.data;
-                    $scope.yo = true;
-                }
-            });
-        };
-
-        $scope.ruta = function(obj) {
-
-            console.log(obj)
-            $scope.rutaCarpeta = obj.path;
-            var dropzone =
-                $('#file-dropzone').dropzone({
-                    url: "C:/Nomina_Timbrado",
-                    maxFilesize: 100,
-                    paramName: "uploadfile",
-                    //Message: "Documentos",
-                    maxThumbnailFilesize: 5,
-                    init: function() {
-
-                        this.on('success', function(file, json) {});
-
-                        this.on('addedfile', function(file) {
-
-                        });
-
-                        this.on('drop', function(file) {
-                            alert('file');
-                        });
-                    }
-                });
-        };
     }
+
+    $scope.getTipoNomina = function() {
+        filtrosRepository.getTipoNomina().then(function(result) {
+            if (result.data.length > 0) {
+                $scope.tipoNomina = result.data;
+            }
+        });
+    }
+
+    $scope.getFileTree = function(idEmpresa, idTipo) {
+        $scope.rutaCarpeta = "";
+        filetreeRepository.getFileTree(idEmpresa, idTipo).then(function(result) {
+            if (result.data != undefined) {
+                $scope.filetree = result.data;
+                $scope.yo = true;
+            }
+        });
+    };
+
+    $scope.ruta = function(obj) {
+    	$scope.nombre = obj.name
+        $("ul").children('#'+$scope.nombre).slideToggle("fast");
+        $scope.rutaCarpeta = obj.path;
+       }
+
 
 });
